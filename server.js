99c62ae4acab573d44da1bbeac2f6de15d09e2b7
Fernando <palhares.f@gmail.com>
2025-02-11 09:58:02 -0300

    Security features implemented

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
+const helmet = require("helmet");
+
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +18,12 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  helmet.frameguard({ action: "sameorigin" }),
+  helmet.dnsPrefetchControl(),
+  helmet.referrerPolicy({ policy: "same-origin" })
+);
 
 //Sample front-end
 app.route('/b/:board/')
@@ -61,5 +69,6 @@ const listener = app.listen(process.env.PORT || 3000, function () {
     }, 1500);
   }
 });
+
 
 module.exports = app; //for testing
